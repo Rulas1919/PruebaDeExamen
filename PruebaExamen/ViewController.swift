5701cd3066ccbd8f0f7dbb69dc722cc4ba74c0f0
Raul <raul-121>
2024-06-13 01:22:07 -0600

    Initial Commit

---
+++ PruebaExamen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaExamen
+//
+//  Created by Jose Raul Gastelum Ruiz on 13/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
